417c393d6eb123227066455fc4812837e8a160f3
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2024-10-08 22:06:07 +0600

    Create quick_sort.cpp

---
+++ DS_Algo/Array/quick_sort.cpp
@@ -0,0 +1,45 @@
+int partition(vector<int>&arr,int low,int high){
+    int pivot=arr[low];
+    int i=low,j=high;
+
+    while(i<j){
+        while(arr[i]<=pivot and i<high){
+            i++;
+        }
+
+        while(arr[j]>=pivot and j>low){
+            j--;
+        }
+        if(i<j){
+            swap(arr[i],arr[j]);
+        }
+    }
+
+    swap(arr[low],arr[j]);
+
+    return j;
+
+}
+
+void qs(vector<int>&arr,int low,int high){
+    if(low<high){
+        int pt=partition(arr,low,high);
+        qs(arr,low,pt-1);
+        qs(arr,pt+1,high);
+    }
+}
+
+
+
+
+
+
+int main() {
+
+    vector<int>arr={4,6,2,5,7,7,1,3};
+    qs(arr,0,arr.size()-1);
+
+    debug(arr);
+    
+    return 0;
+}
